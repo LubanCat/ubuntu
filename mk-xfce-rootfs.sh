7ebd953e8f443cd5efa876ba10eac55a741db5d3
hejiawencc <hjw0415@outlook.com>
2023-04-10 15:08:09 +0800

    rootfs 同步ubuntu20.04分支，并完善构建脚本

--- mk-xfce-rootfs.sh
+++ mk-xfce-rootfs.sh
@@ -64,13 +64,18 @@ case "${ARCH:-$1}" in
 		;;
 esac
 
+if [ ! $TARGET ]; then
+	TARGET='xfce'
+    echo -e "\033[47;36m set default TARGET=xfce...... \033[0m"
+fi
+
 echo -e "\033[47;36m Building for $ARCH \033[0m"
 
 if [ ! $VERSION ]; then
 	VERSION="release"
 fi
 
-if [ ! -e ubuntu-base-lite-$ARCH-*.tar.gz ]; then
+if [ ! -e ubuntu-base-$TARGET-$ARCH-*.tar.gz ]; then
 	echo "\033[41;36m Run mk-base-ubuntu.sh first \033[0m"
 	exit -1
 fi
@@ -83,7 +88,7 @@ trap finish ERR
 
 echo -e "\033[47;36m Extract image \033[0m"
 sudo rm -rf $TARGET_ROOTFS_DIR
-sudo tar -xpf ubuntu-base-lite-$ARCH-*.tar.gz
+sudo tar -xpf ubuntu-base-$TARGET-$ARCH-*.tar.gz
 
 # packages folder
 sudo mkdir -p $TARGET_ROOTFS_DIR/packages
@@ -146,16 +151,59 @@ chmod +x /etc/rc.local
 export APT_INSTALL="apt-get install -fy --allow-downgrades"
 
 echo -e "\033[47;36m ---------- LubanCat -------- \033[0m"
+\apt-get remove -y gnome-bluetooth
 \${APT_INSTALL} gdisk parted bluez bluez-tools bc xinput
 
+#Desktop background picture
+ln -sf /usr/share/xfce4/backdrops/lubancat-wallpaper.png /usr/share/xfce4/backdrops/xubuntu-wallpaper.png
 
 apt install -fy --allow-downgrades /packages/install_packages/*.deb
+
+echo -e "\033[47;36m ----- power management ----- \033[0m"
+\${APT_INSTALL} pm-utils triggerhappy bsdmainutils
+cp /etc/Powermanager/triggerhappy.service  /lib/systemd/system/triggerhappy.service
+
+echo -e "\033[47;36m ------ Setup Video---------- \033[0m"
+\${APT_INSTALL} gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-alsa \
+gstreamer1.0-plugins-base-apps qtmultimedia5-examples
+
+\${APT_INSTALL} /packages/mpp/*
+\${APT_INSTALL} /packages/gst-rkmpp/*.deb
+\${APT_INSTALL} /packages/gstreamer/*.deb
+# \${APT_INSTALL} /packages/gst-plugins-base1.0/*.deb
+# \${APT_INSTALL} /packages/gst-plugins-bad1.0/*.deb
+# \${APT_INSTALL} /packages/gst-plugins-good1.0/*.deb
+
+echo -e "\033[47;36m ----- Install Camera ----- - \033[0m"
+\${APT_INSTALL} cheese v4l-utils
+\${APT_INSTALL} /packages/libv4l/*.deb
+
+echo -e "\033[47;36m ----- Install Xserver------- \033[0m"
+\${APT_INSTALL} /packages/xserver/*.deb
+apt-mark hold xserver-common xserver-xorg-core xserver-xorg-legacy
+
+echo -e "\033[47;36m ------ update chromium ----- \033[0m"
+\${APT_INSTALL} /packages/chromium/*.deb
+# fix GLIBCXX_3.4.30 not found
+ln -sf /lib/aarch64-linux-gnu/libstdc++.so.6 /usr/lib/chromium/libstdc++.so.6
+
+# echo -e "\033[47;36m --------- firefox-esr ------ \033[0m"
+# \${APT_INSTALL} /packages/firefox/*.deb
 
 echo -e "\033[47;36m ------- Install libdrm ------ \033[0m"
 \${APT_INSTALL} /packages/libdrm/*.deb
 
-# HACK to disable the kernel logo on bootup
-sed -i "/exit 0/i \ echo 3 > /sys/class/graphics/fb0/blank" /etc/rc.local
+echo -e "\033[47;36m ------ libdrm-cursor -------- \033[0m"
+\${APT_INSTALL} /packages/libdrm-cursor/*.deb
+
+# Only preload libdrm-cursor for X
+sed -i "/libdrm-cursor.so/d" /etc/ld.so.preload
+sed -i "1aexport LD_PRELOAD=libdrm-cursor.so.1" /usr/bin/X
+
+if [ "$VERSION" == "debug" ]; then
+echo -e "\033[47;36m ------ Install glmark2 ------ \033[0m"
+\${APT_INSTALL} glmark2-es2
+fi
 
 if [ -e "/usr/lib/aarch64-linux-gnu" ] ;
 then
@@ -165,6 +213,36 @@ fi
 
 echo -e "\033[47;36m ----- Install rktoolkit ----- \033[0m"
 \${APT_INSTALL} /packages/rktoolkit/*.deb
+
+# # echo -e "\033[47;36m Install Chinese fonts.................... \033[0m"
+# # # Uncomment zh_CN.UTF-8 for inclusion in generation
+# # sed -i 's/^# *\(zh_CN.UTF-8\)/\1/' /etc/locale.gen
+# # echo "LANG=zh_CN.UTF-8" >> /etc/default/locale
+
+# # # Generate locale
+# # locale-gen
+
+# # # Export env vars
+# # echo "export LC_ALL=zh_CN.UTF-8" >> ~/.bashrc
+# # echo "export LANG=zh_CN.UTF-8" >> ~/.bashrc
+# # echo "export LANGUAGE=zh_CN.UTF-8" >> ~/.bashrc
+
+# # source ~/.bashrc
+
+# #ln -sf /usr/bin/startxfce4 /etc/alternatives/x-session-manager
+
+# # mark package to hold
+# apt list --installed | grep -v oldstable | cut -d/ -f1 | xargs apt-mark hold
+
+echo -e "\033[47;36m ------ Install ffmpeg ------- \033[0m"
+\${APT_INSTALL} ffmpeg
+# \${APT_INSTALL} /packages/ffmpeg/*.deb
+
+# echo -e "\033[47;36m ------- Install mpv --------- \033[0m"
+# \${APT_INSTALL} /packages/mpv/*.deb
+
+# HACK to disable the kernel logo on bootup
+sed -i "/exit 0/i \ echo 3 > /sys/class/graphics/fb0/blank" /etc/rc.local
 
 echo -e "\033[47;36m ------- Custom Script ------- \033[0m"
 systemctl mask systemd-networkd-wait-online.service
@@ -188,6 +266,7 @@ then
 fi
 cd -
 
+apt autoremove -y
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/cache/
 rm -rf /packages/
@@ -196,4 +275,4 @@ EOF
 
 sudo umount $TARGET_ROOTFS_DIR/dev
 
-IMAGE_VERSION=lite ./mk-image.sh 
+IMAGE_VERSION=$TARGET ./mk-image.sh 
