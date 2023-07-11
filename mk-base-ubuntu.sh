b84edfcb57ee3fd503f57e3e9f6521a49c16f01d
hejiawencc <hjw0415@outlook.com>
2023-07-11 16:29:18 +0800

    base pip使用aliyun镜像地址安装软件包

--- mk-base-ubuntu.sh
+++ mk-base-ubuntu.sh
@@ -169,7 +169,7 @@ elif [ "$TARGET" == "lite" ]; then
     \${APT_INSTALL}  
 fi
 
-pip3 install python-periphery Adafruit-Blinka
+pip3 install python-periphery Adafruit-Blinka -i https://mirrors.aliyun.com/pypi/simple/
 
 HOST=lubancat
 
