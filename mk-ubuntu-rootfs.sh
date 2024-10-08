a72d658a6308f3c16f59061fbe787cb5b0291fe6
hejiawencc <hjw0415@outlook.com>
2024-10-08 14:10:43 +0800

    rootfs 移除预装软件initramfs-tools

--- mk-ubuntu-rootfs.sh
+++ mk-ubuntu-rootfs.sh
@@ -221,6 +221,8 @@ if [ $MIRROR ]; then
     \${APT_INSTALL} fire-config lbc-test
 fi
 
+apt purge initramfs-tools -y
+
 \${APT_INSTALL} u-boot-tools edid-decode logrotate
 if [[ "$TARGET" == "gnome" || "$TARGET" == "gnome-full" ]]; then
     \${APT_INSTALL} gdisk
