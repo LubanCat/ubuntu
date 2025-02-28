4cdbfb596e211dcc5ee5288ed67375fd6af3c8b9
hejiawencc <hjw0415@outlook.com>
2025-02-28 11:42:11 +0800

    rootfs 添加lbc-test测试工具

--- mk-ubuntu-rootfs.sh
+++ mk-ubuntu-rootfs.sh
@@ -202,7 +202,7 @@ chmod +x /etc/rc.local
 export APT_INSTALL="apt-get install -fy --allow-downgrades"
 
 echo -e "\033[47;36m ---------- LubanCat -------- \033[0m"
-\${APT_INSTALL} fire-config u-boot-tools logrotate
+\${APT_INSTALL} fire-config u-boot-tools logrotate lbc-test
 if [[ "$TARGET" == "gnome" || "$TARGET" == "gnome-full" ]]; then
     \${APT_INSTALL} gdisk fire-config-gui
     #Desktop background picture
