4d8ccce2291f6400e0b8c767f1f70771f8c594e2
hejiawencc <hjw0415@outlook.com>
2025-03-22 15:55:00 +0800

    overlay 修复usbdevice脚本，解决ums挂载问题

--- overlay/etc/profile.d/usbdevice.sh
+++ overlay/etc/profile.d/usbdevice.sh
@@ -5,7 +5,7 @@
 # option: adb acm hid mtp ntb rndis uac1 uac2 ums uvc
 export USB_FUNCS="rndis"
 
-export UMS_FILE=/userdata/ums_shared.img
+export UMS_FILE=/ums_shared.img
 export UMS_SIZE=256M
 export UMS_FSTYPE=vfat
 export UMS_MOUNT=0
