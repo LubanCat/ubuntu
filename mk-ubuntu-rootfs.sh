92346e4a4e56dd03ca4d847677c25f6bd69e358a
hejiawencc <hjw0415@outlook.com>
2026-05-21 15:52:37 +0800

    rootfs xfce卸载gdm3，使用lightdm登录

--- mk-ubuntu-rootfs.sh
+++ mk-ubuntu-rootfs.sh
@@ -231,6 +231,7 @@ elif [[ "$TARGET" == "xfce" || "$TARGET" == "xfce-full" ]]; then
     \apt-get remove -y gnome-bluetooth
     \${APT_INSTALL} bluez bluez-tools
     [[-z $MIRROR ]] && \${APT_INSTALL} fire-config-gui
+    apt remove -fy gdm3
     #Desktop background picture
     ln -sf /usr/share/xfce4/backdrops/lubancat-wallpaper.png /usr/share/xfce4/backdrops/xubuntu-wallpaper.png
     ln -sf /usr/share/xfce4/backdrops/lubancat-wallpaper.png /usr/share/backgrounds/xfce/xfce-stripes.png
